9de4e66216eb5ef2cdf33f6e4fbf0f556febde7d
Raditz Farhan <raditzfarhan@gmail.com>
2026-07-09 23:07:50 +0800

    feat: add TngClient orchestrating signing, dispatch, and verification

---
+++ src/Client/TngClient.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Laraditz\TngEwallet\Client;
+
+use Laraditz\TngEwallet\Client\Concerns\HandlesErrors;
+use Laraditz\TngEwallet\Client\Concerns\HandlesSigning;
+use Laraditz\TngEwallet\Client\Concerns\MakesHttpRequests;
+use Laraditz\TngEwallet\Client\Concerns\VerifiesResponseSignature;
+use Laraditz\TngEwallet\Client\Contracts\ClientInterface;
+
+class TngClient implements ClientInterface
+{
+    use HandlesErrors;
+    use HandlesSigning;
+    use MakesHttpRequests;
+    use VerifiesResponseSignature;
+
+    public function post(string $uri, array $data): array
+    {
+        $clientId = config('tng-ewallet.client_id');
+        $requestTime = $this->generateRequestTime();
+        $body = json_encode($data);
+
+        $content = $this->buildContentToBeSigned($uri, $clientId, $requestTime, $body);
+        $signature = $this->sign($content, config('tng-ewallet.private_key_path'));
+        $headers = $this->buildSigningHeaders($clientId, $requestTime, (int) config('tng-ewallet.key_version'), $signature);
+
+        $response = $this->newRequest()
+            ->withHeaders($headers)
+            ->withBody($body, 'application/json; charset=UTF-8')
+            ->post($uri);
+
+        $this->assertSuccessfulResponse($response);
+
+        if (config('tng-ewallet.verify_response_signature')) {
+            $this->assertValidSignature(
+                $uri,
+                $response->header('Client-Id'),
+                $response->header('Response-Time'),
+                $response->body(),
+                $this->extractSignatureValue($response->header('Signature')),
+                file_get_contents(config('tng-ewallet.public_key_path')),
+            );
+        }
+
+        return $response->json();
+    }
+
+    protected function extractSignatureValue(string $signatureHeader): string
+    {
+        preg_match('/signature=(.+)$/', $signatureHeader, $matches);
+
+        return $matches[1] ?? '';
+    }
+}
