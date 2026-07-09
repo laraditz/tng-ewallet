a5d89ed2e70d71b94423d81a3b65a4d4c54c5a20
Raditz Farhan <raditzfarhan@gmail.com>
2026-07-09 23:29:27 +0800

    feat: add AuthorizationService::applyToken() API call

--- src/Services/AuthorizationService.php
+++ src/Services/AuthorizationService.php
@@ -3,6 +3,7 @@
 namespace Laraditz\TngEwallet\Services;
 
 use Laraditz\TngEwallet\Client\Contracts\ClientInterface;
+use Laraditz\TngEwallet\Responses\ApplyTokenResponse;
 use Laraditz\TngEwallet\Responses\PrepareResponse;
 
 class AuthorizationService
@@ -14,5 +15,10 @@ class AuthorizationService
     public function prepare(array $data): PrepareResponse
     {
         return new PrepareResponse($this->client->post('/v1/authorizations/prepare', $data));
+    }
+
+    public function applyToken(array $data): ApplyTokenResponse
+    {
+        return new ApplyTokenResponse($this->client->post('/v1/authorizations/applyToken', $data));
     }
 }
