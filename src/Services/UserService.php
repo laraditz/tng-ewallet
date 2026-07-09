ef44ee88b0adf6355ce617cf0dd0f2357d9ffbe5
Raditz Farhan <raditzfarhan@gmail.com>
2026-07-09 23:53:50 +0800

    feat: upsert TngUser row on inquiryByAccessToken(), keyed on userId

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace Laraditz\TngEwallet\Services;
 
 use Laraditz\TngEwallet\Client\Contracts\ClientInterface;
+use Laraditz\TngEwallet\Models\TngUser;
 use Laraditz\TngEwallet\Responses\UserInfoResponse;
 
 class UserService
@@ -13,6 +14,20 @@ class UserService
 
     public function inquiryByAccessToken(array $data): UserInfoResponse
     {
-        return new UserInfoResponse($this->client->post('/v1/customers/user/inquiryUserInfoByAccessToken', $data));
+        $response = new UserInfoResponse($this->client->post('/v1/customers/user/inquiryUserInfoByAccessToken', $data));
+
+        $userId = $response->userInfo['userId'] ?? null;
+
+        if ($userId !== null) {
+            TngUser::updateOrCreate(
+                ['user_id' => $userId],
+                [
+                    'user_info' => $response->userInfo,
+                    'last_fetched_at' => now(),
+                ],
+            );
+        }
+
+        return $response;
     }
 }
